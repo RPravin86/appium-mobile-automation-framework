2e8774649a7eca93a0cf4ea3f9d60b4e5f94c8ad
rPravin86 <62685733+RPravin86@users.noreply.github.com>
2026-09-19 10:18:47 +0530

    fix: remove test discovery warnings

--- src/test/java/io/github/rpravin86/mobile/runner/RunCucumberTest.java
+++ src/test/java/io/github/rpravin86/mobile/runner/RunCucumberTest.java
@@ -2,7 +2,7 @@ package io.github.rpravin86.mobile.runner;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
@@ -11,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectPackages("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.rpravin86.mobile")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @ignore")
 @ConfigurationParameter(
